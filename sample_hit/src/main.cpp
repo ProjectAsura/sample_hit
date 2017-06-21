22d4123ca40d04c5052de645edb9a5fff20b0bc1
Pocol <project.asura@gmail.com>
2017-06-22 08:22:23 +0900

    追加。

---
+++ sample_hit/src/main.cpp
@@ -0,0 +1,155 @@
+﻿//-------------------------------------------------------------------------------------------------
+// File : main.cpp
+// Desc : Main Entry Point.
+// Copyright(c) Project Asura. All right reserved.
+//-------------------------------------------------------------------------------------------------
+
+//-------------------------------------------------------------------------------------------------
+// Inlcudes
+//-------------------------------------------------------------------------------------------------
+#include <r3d_math.h>
+#include <r3d_camera.h>
+#include <r3d_shape.h>
+#include <vector>
+#include <stb/stb_image_write.h>
+
+
+namespace {
+
+//-------------------------------------------------------------------------------------------------
+// Global Varaibles.
+//-------------------------------------------------------------------------------------------------
+const int     g_max_depth = 5;
+const Vector3 g_light_pos   (50.0,  75.0,   81.6);
+const Vector3 g_light_color (256.0, 256.0, 256.0);
+const Vector3 g_back_ground (0.0,   0.0,    0.0);
+const Sphere  g_spheres[] = {
+    Sphere(1e5,     Vector3( 1e5 + 1.0,    40.8,          81.6), Vector3(0.25,  0.75,  0.25)),
+    Sphere(1e5,     Vector3(-1e5 + 99.0,   40.8,          81.6), Vector3(0.25,  0.25,  0.75)),
+    Sphere(1e5,     Vector3(50.0,          40.8,           1e5), Vector3(0.75,  0.75,  0.75)),
+    Sphere(1e5,     Vector3(50.0,          40.8,  -1e5 + 170.0), Vector3()                  ),
+    Sphere(1e5,     Vector3(50.0,           1e5,          81.6), Vector3(0.75,  0.75,  0.75)),
+    Sphere(1e5,     Vector3(50.0,   -1e5 + 81.6,          81.6), Vector3(0.75,  0.75,  0.75)),
+    Sphere(16.5,    Vector3(27.0,          16.5,          47.0), Vector3(0.75,  0.25,  0.25)),
+    Sphere(16.5,    Vector3(73.0,          16.5,          78.0), Vector3(0.99,  0.99,  0.99))
+};
+
+
+//-------------------------------------------------------------------------------------------------
+//      シーンとの交差判定を行います.
+//-------------------------------------------------------------------------------------------------
+inline bool intersect_scene(const Ray& ray, double* t, int* id)
+{
+    auto n = static_cast<int>(sizeof(g_spheres) / sizeof(g_spheres[0]));
+
+    *t  = D_MAX;
+    *id = -1;
+
+    for (auto i = 0; i < n; ++i)
+    {
+        auto d = g_spheres[i].intersect(ray);
+        if (d > D_HIT_MIN && d < *t)
+        {
+            *t  = d;
+            *id = i;
+        }
+    }
+
+    return (*t < D_HIT_MAX);
+}
+
+//-------------------------------------------------------------------------------------------------
+//      交差物体の色を求めます.
+//-------------------------------------------------------------------------------------------------
+Vector3 shade(const Ray& ray)
+{
+    double t;
+    int   id;
+
+    // シーンとの交差判定.
+    if (!intersect_scene(ray, &t, &id))
+    { return g_back_ground; }
+
+    // 交差物体の色を返却.
+    return g_spheres[id].color;
+}
+
+//-------------------------------------------------------------------------------------------------
+//      BMPファイルに保存します.
+//-------------------------------------------------------------------------------------------------
+void save_to_bmp(const char* filename, int width, int height, const double* pixels)
+{
+    std::vector<uint8_t> images;
+    images.resize(width * height * 3);
+
+    const double inv_gamma = 1.0 / 2.2;
+
+    for(auto i=0; i<width * height * 3; i+=3)
+    {
+        auto r = pow(pixels[i + 0], inv_gamma);
+        auto g = pow(pixels[i + 1], inv_gamma);
+        auto b = pow(pixels[i + 2], inv_gamma);
+
+        r = saturate(r);
+        g = saturate(g);
+        b = saturate(b);
+
+        images[i + 0] = static_cast<uint8_t>( r * 255.0 + 0.5 );
+        images[i + 1] = static_cast<uint8_t>( g * 255.0 + 0.5 );
+        images[i + 2] = static_cast<uint8_t>( b * 255.0 + 0.5 );
+    }
+
+    stbi_write_bmp(filename, width, height, 3, images.data());
+}
+
+} // namespace
+
+
+//-------------------------------------------------------------------------------------------------
+//      メインエントリーポイントです.
+//-------------------------------------------------------------------------------------------------
+int main(int argc, char** argv)
+{
+    // レンダーターゲットのサイズ.
+    int width  = 640;
+    int height = 480;
+
+    // カメラ用意.
+    Camera camera(
+        Vector3(50.0, 52.0, 295.6),                 // カメラ位置.
+        normalize(Vector3(0.0, -0.042612, -1.0)),   // 視線ベクトル.
+        Vector3(0.0, 1.0, 0.0),                     // 注視点.
+        0.5135,                                     // 垂直画角(rad)
+        double(width) / double(height),             // アスペクト比.
+        130.0                                       // スクリーンまでの距離.
+    );
+
+    // レンダーターゲット生成.
+    std::vector<Vector3> image;
+    image.resize(width * height);
+
+    // レンダーターゲットをクリア.
+    for (size_t i = 0; i < image.size(); ++i)
+    { image[i] = g_back_ground; }
+
+    for (auto y = 0; y < height; ++y)
+    {
+        for (auto x = 0; x < width; ++x)
+        {
+            auto idx = y * width + x;
+            auto fx = double(x) / double(width)  - 0.5;
+            auto fy = double(y) / double(height) - 0.5;
+
+            // Let's レイトレ！
+            image[idx] += shade(camera.emit(fx, fy));
+        }
+    }
+
+    // レンダーターゲットの内容をファイルに保存.
+    save_to_bmp("image.bmp", width, height, &image.data()->x);
+
+    // レンダーターゲットクリア.
+    image.clear();
+
+    return 0;
+}
